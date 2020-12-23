3d882ce632893873216b8daef55e478db8e75c8e
Antoine <antoine.brld@gmail.com>
2020-12-23 18:36:31 +0100

    init

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,9 @@
+
 import Head from 'next/head'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import React from "react";
 
 const name = '[Your Name]'
 export const siteTitle = 'Next.js Sample Website'
